c711a5b5b76cb64be6eb99edacb4a430f6651fc2
Shevaughn-Smith <Shevaughnssmith@gmail.com>
2022-10-13 14:09:07 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,24 @@
 window.onload = init;
+var gamePlay = new Array();
 
 function init() {
 	var button = document.getElementsByClassName("btn")[0].setAttribute("onclick", "createBoard()")
+}
+
+function markSquare(val) {
+	if (gamePlay.length == 0) {
+		gamePlay.push("X")
+		val.setAttribute("class", "square X")
+		val.textContent = "X"
+	} else if (gamePlay[gamePlay.length - 1] == "O") {
+		gamePlay.push("X")
+		val.setAttribute("class", "square X")
+		val.textContent = "X"
+	} else {
+		gamePlay.push("O")
+		val.setAttribute("class", "square O")
+		val.textContent = "O"
+	}
 }
 
 
@@ -11,7 +28,10 @@ function createBoard() {
 
 	for (var i=0; i < children.length; i++) {
 	  if (children[i].nodeName == "DIV") {
-	    children[i].setAttribute("class", "square") 
+	    children[i].setAttribute("class", "square")
+	    children[i].setAttribute("onclick", "markSquare(this)")
 	  }
 	} 
 }
+
+
